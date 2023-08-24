935cd9d2b3e2d2ac827ec8ae9070d06d71384105
shakibnymr <shakibnymr11@gmail.com>
2023-08-24 17:22:47 +0600

    map

---
+++ more-map.js
@@ -0,0 +1,7 @@
+const numbers = [12, 10, 8,15, 7];
+const doubled = numbers.map(n => n/2);
+// console.log(doubled);
+
+const friends = ['tom', 'jon', 'mikel', 'oliv']
+const lenghts = friends.map( friend => friend[0]);
+console.log(lenghts)
